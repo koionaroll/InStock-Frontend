8b3781223167b14bc2da7cddb19b6f6405420e81
Khoi Tran <tranvankhoi2002@gmail.com>
2023-03-21 15:11:13 -0700

    first commit on dev branch

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <div>Test</div>
         <a
           className="App-link"
           href="https://reactjs.org"
