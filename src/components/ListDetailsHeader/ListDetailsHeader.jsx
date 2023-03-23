5c6f607c4f52745346378a8dac124bfdb44c0c24
Khoi Tran <tranvankhoi2002@gmail.com>
2023-03-23 11:59:11 -0700

    fixed header list

--- src/components/ListDetailsHeader/ListDetailsHeader.jsx
+++ src/components/ListDetailsHeader/ListDetailsHeader.jsx
@@ -2,62 +2,54 @@ import React from "react";
 import backArrow from "../../assets/Icons/arrow_back-24px.svg";
 import edit from "../../assets/Icons/edit-24px.svg";
 import "./ListDetailsHeader.scss";
+import sort from "../../assets/Icons/sort-24px.svg";
 
 function ListDetailsHeader({ prop }) {
   return (
     <>
+        <div className="header--background">
       <div className="header--container">
-        <div className="header--location-container">
-          <img className="header--back" src={backArrow} alt="" />
-          <h1 className="header--location">
-            Washington
-            {
-              // prop.title
-            }
-          </h1>
+          <div className="header--location-container">
+            <img className="header--back" src={backArrow} alt="" />
+            <h1 className="header--location">Washington</h1>
+          </div>
+          <img className="header--edit" src={edit} alt="" />
         </div>
-        <img className="header--edit" src={edit} alt="" />
       </div>
       <div className="contact--container">
         <div>
           <p className="contact--label">WAREHOUSE ADDRESS:</p>
           <p className="contact--details">
             33 Pearl Street SW, Washington, USA
-            {
-              //prop.address
-            }
           </p>
         </div>
-        <div>
-          <p className="contact--label">CONTACT NAME:</p>
-          <p className="contact--details">
-            Graeme Lyon{" "}
-            {
-              //prop.name
-            }
-          </p>
-          <p className="contact--details">
-            Warehouse Manager{" "}
-            {
-              //prop.role
-            }
-          </p>
+        <div className="contact--container--tablet">
+          <div>
+            <p className="contact--label">CONTACT NAME:</p>
+            <p className="contact--details">Graeme Lyon</p>
+            <p className="contact--details">Warehouse Manager</p>
+          </div>
+          <div>
+            <p className="contact--label">CONTACT INFORMATION:</p>
+            <p className="contact--details">+1 (647) 504-0911</p>
+            <p className="contact--details">glyon@instock.com</p>
+          </div>
         </div>
-        <div>
-          <p className="contact--label">CONTACT INFORMATION:</p>
-          <p className="contact--details">
-            +1 (647) 504-0911{" "}
-            {
-              //prop.phone
-            }
-          </p>
-          <p className="contact--details">
-            glyon@instock.com{" "}
-            {
-              //prop.email
-            }
-          </p>
-        </div>
+      </div>
+      <div className="details--label--container">
+        <p className="details--label--tablet">
+          INVENTORY ITEM <img src={sort} alt="" />
+        </p>
+        <p className="details--label--tablet">
+          CATEGORY <img src={sort} alt="" />
+        </p>
+        <p className="details--label--tablet">
+          STATUS <img src={sort} alt="" />
+        </p>
+        <p className="details--label--tablet">
+          QUANTITY <img src={sort} alt="" />
+        </p>
+        <p className="details--label--tablet--action">ACTIONS</p>
       </div>
     </>
   );
--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -1,53 +1,44 @@
-import React from 'react'
+import React from "react";
 import trash from "../../assets/Icons/delete_outline-24px.svg";
 import edit from "../../assets/Icons/edit-24px.svg";
-import "./ListItem.scss"
+import "./ListItem.scss";
+
 
 function ListItem() {
+    
   return (
     <>
       <div className="details--container">
-        <div>
+        <div className="details--label--tablet">
           <p className="details--label">INVENTORY ITEM</p>
-          <p className="details--info">
-            Televisions
-            {
-              //prop.address
-            }
-          </p>
+          <p className="details--info">Televisions</p>
         </div>
-        <div>
-          <p className="details--label">STATUS</p>
-          <p className="details--info">
-            INSTOCK
-            {
-              //prop.address
-            }
-          </p>
-        </div>
-        <div>
+        <div className="details--label--tablet">
           <p className="details--label">CATEGORY</p>
-          <p className="details--info">
-            Electronics{" "}
-            {
-              //prop.name
-            }
-          </p>
+          <p className="details--info">Electronics </p>
         </div>
-        <div>
+        <div className="details--label--tablet">
+          <p className="details--label">STATUS</p>
+          <p className="details--info">INSTOCK</p>
+        </div>
+        <div className="details--label--tablet">
           <p className="details--label">QUANTITY</p>
-          <p className="details--info">
-            500
-            {
-              //prop.email
-            }
-          </p>
+          <p className="details--info">500</p>
         </div>
-        <button className='delete'><img src={trash} alt="" /></button>
-        <button className='edit'><img src={edit} alt="" /></button>
+        <div className=" button--container">
+          <button className="button--delete">
+            <img src={trash} alt="" />
+          </button>
+          <button className="button--edit-1">
+            <img src={edit} alt="" />
+          </button>
         </div>
+        <button className="button--edit-2">
+          <img src={edit} alt="" />
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default ListItem
+export default ListItem;
