1b3483a45debfd8da0e7e60dbca4fef5de1e2416
cmsanchezg <sanchez.carlomg@gmail.com>
2023-03-24 10:28:56 -0700

    Inventory Item Details Component (Responsive)

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,25 @@ import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
 import WarehouseDetails from './pages/WarehouseDetails/WarehouseDetails';
+import InventoryItemDetails from './components/InventoryItemDetails/InventoryItemDetails';
 
 
 function App() {
   return (
     <BrowserRouter>
-    {/* <Header /> */}
+    <Header />
     <Routes>
-{/*       
-      <Route path="/warehouses" element={<Warehouses />} />
-      <Route path="/warehouses/add" element={<Warehouse />} /> */}
+      
+      {/* <Route path="/warehouses" element={<Warehouses />} /> */}
+      {/* <Route path="/warehouses/add" element={<Warehouse />} /> */}
       <Route path="/warehouses/:warehouseId" element={<WarehouseDetails />} />
-      {/* <Route path="/warehouses/edit/:warehouseId" element={<Warehouse />} />
-      <Route path="/inventory" element={<Inventory />} />
-      <Route path="/inventory/:itemId" element={<Inventory />} />
-      <Route path="/inventory/add" element={<Inventory />} />
-      <Route path="/inventory/edit/:itemId" element={<Inventory />} /> */}
+      {/* <Route path="/warehouses/edit/:warehouseId" element={<Warehouse />} /> */}
+      {/* <Route path="/inventory" element={<Inventory />} /> */}
+      <Route path="/inventory/:itemId" element={<InventoryItemDetails />} />
+      {/* <Route path="/inventory/add" element={<Inventory />} /> */}
+      {/* <Route path="/inventory/edit/:itemId" element={<Inventory />} /> */}
     </Routes>
-    {/* <Footer /> */}
+    <Footer />
     </BrowserRouter>
   );
 }
