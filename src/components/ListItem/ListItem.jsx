8c44fce63dd4e8c66e3d8d2915565d26195785a7
Khoi Tran <tranvankhoi2002@gmail.com>
2023-03-25 16:12:44 -0700

    added dynamic item page and links

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -4,9 +4,10 @@ import { ReactComponent as Edit } from "../../assets/Icons/edit-24px.svg";
 import chevron from "../../assets/Icons/chevron_right-24px.svg";
 import "./ListItem.scss";
 import { useState, useEffect } from "react";
-import {Link} from "react-router-dom";
+import {Link} from "react-router-dom"
 
-function ListItem({ element }) {
+
+function ListItem({ element, warehouseInfo }) {
   const [inStock, setStock] = useState(false);
   useEffect(() => {
     if (element.quantity === 0) {
@@ -15,13 +16,12 @@ function ListItem({ element }) {
       setStock(true);
     }
   }, [element.quantity]);
-
   return (
     <div className="details--container">
       <div className="details--label--tablet">
-        <Link to="/inventory/:itemId" className="details--info-item"> 
+        <Link to={`/inventory/${element.id}`} state={warehouseInfo} className="details--info-item"> 
         <p className="details--label">INVENTORY ITEM</p>
-        <p className="details--info-item">
+        <p className="details--info-item--row">
           {element.item_name}
           <img src={chevron} alt="" />
         </p>
