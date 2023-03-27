cdb173030b3c1b73ea80bab270d591d34a153c1d
jvpoly <54869526+jvpoly@users.noreply.github.com>
2023-03-26 17:00:46 -0700

    warehouse list ticket recommit & addwarehouse linked

--- src/components/PageMain/PageMain.jsx
+++ src/components/PageMain/PageMain.jsx
@@ -2,27 +2,32 @@ import { Link } from "react-router-dom";
 import "../PageMain/PageMain.scss";
 import { useState } from "react";
 
-const PageMain = ({pageName}) => {
-
-  
-    return (
-        <>
-            <section className="main__section">
-                <div className="main__section-title-container">
-                    <h1 className="main__section-title-text"> {`${pageName === "Warehouse" ? "Warehouses" : "Inventory"}`} </h1>
-                </div>
-                <div className="main__section-input-container">
-                    <input className="main__section-input" placeholder="Search..." type="text"/> 
-                </div>
-                <div className="main__section-button-container">
-                    {/* <button className="main__section-button"> + Add New {pageName} </button> */}
-                    <Link className="main__section-button" to="/warehouses/add">  
-                    + Add New {`${pageName === "Warehouse" ? "Warehouse" : "Item"}`}  
-                    </Link>
-                </div>
-            </section>
-        </>
-    );
-}
+const PageMain = ({ pageName }) => {
+  return (
+    <>
+      <section className="main__section">
+        <div className="main__section-title-container">
+          <h1 className="main__section-title-text">
+            {" "}
+            {`${pageName === "Warehouse" ? "Warehouses" : "Inventory"}`}{" "}
+          </h1>
+        </div>
+        <div className="main__section-input-container">
+          <input
+            className="main__section-input"
+            placeholder="Search..."
+            type="text"
+          />
+        </div>
+        <div className="main__section-button-container">
+          {/* <button className="main__section-button"> + Add New {pageName} </button> */}
+          <Link className="main__section-button" to="/warehouse/add">
+            + Add New {`${pageName === "Warehouse" ? "Warehouse" : "Item"}`}
+          </Link>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default PageMain;
